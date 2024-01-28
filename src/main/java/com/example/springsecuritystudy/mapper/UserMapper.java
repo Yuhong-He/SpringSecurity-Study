62c50ec2f503fcce483da3d38ae000baa6d8a67d
Yuhong He <71640320+Theodore-Ho@users.noreply.github.com>
2024-01-28 22:11:48 +0000

    start

---
+++ src/main/java/com/example/springsecuritystudy/mapper/UserMapper.java
@@ -0,0 +1,7 @@
+package com.example.springsecuritystudy.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.springsecuritystudy.domain.User;
+
+public interface UserMapper extends BaseMapper<User> {
+}
